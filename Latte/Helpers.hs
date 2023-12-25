b9ea88fc94601e8d32a07b0c6355ad7597a2225c
BlazejUW <b.palkus@student.uw.edu.pl>
2023-12-25 10:14:08 +0100

    Przywrócono katalog Latte, poprawiono readme

---
+++ Latte/Helpers.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+
+module Latte.Helpers where
+
+import qualified Latte.Abs
+
+data Type = Integer | String | Boolean | Void deriving (Eq, Ord, Show) --tutaj pomyslec co z voidem i z fun
+
+typeToKeyword :: Type -> String 
+typeToKeyword = \case
+  String -> "string"
+  Integer -> "int"
+  Boolean -> "boolean"
+  Void -> "void"
+
+keywordToType :: Latte.Abs.Type -> Type
+keywordToType = \case
+  Latte.Abs.Int _ -> Integer
+  Latte.Abs.Str _ -> String
+  Latte.Abs.Bool _ -> Boolean
+  Latte.Abs.Void _ -> Void
+
+typeToLlvmKeyword :: Type -> String 
+typeToLlvmKeyword = \case
+  Integer -> "i32"
+  String -> "i8*" 
+  Boolean -> "i1"
+  Void -> "void"
+
+
+name :: Latte.Abs.Ident -> String
+name (Latte.Abs.Ident s) = s
+
+functionName :: (Latte.Abs.Ident, [Type]) -> String
+functionName (Latte.Abs.Ident s, ts) = s ++ "(" ++ concatMap show ts ++ ")"
+
+errLocation :: (Show a1, Show a2) => Maybe (a1, a2) -> String
+errLocation p = case p of
+  Nothing -> "at unknown location"
+  Just (line, col) -> "at line " ++ show line ++ ", column " ++ show col
---
+++ Latte/RunCompile.hs
@@ -0,0 +1,99 @@
+module Main where
+
+import Prelude
+  ( ($), (.)
+  , Either(..)
+  , Int, (>)
+  , String, (++), concat, unlines
+  , Show, show
+  , IO, (>>), (>>=), mapM_, putStrLn, putStr
+  , FilePath
+  , getContents, readFile, return, reverse
+  )
+import System.Environment ( getArgs )
+import System.Exit        ( exitFailure, exitWith )
+import Control.Monad      ( when )
+
+import qualified Latte.Abs
+import Latte.Abs   ()
+import Latte.Lex   ( Token, mkPosToken )
+import Latte.Par   ( pProgram, myLexer )
+import Latte.Print ( Print, printTree )
+--TODO compiler
+import Latte.Typechecker (runTypechecker, Typecheck, TypecheckerState)
+import Latte.Compiler (Compile, runCompiler, compilerOutput, CompilerState)
+
+type Err        = Either String
+type ParseFun a = [Token] -> Err a
+type Verbosity  = Int
+
+putStrV :: Verbosity -> String -> IO ()
+putStrV v s = when (v > 1) $ putStrLn s
+
+runFile :: (Print a, Show a, Compile a, Typecheck a) => Verbosity -> ParseFun a -> FilePath -> IO ()
+runFile v p f = putStrV v f >> readFile f >>= run v p
+
+run :: (Print a, Show a, Compile a, Typecheck a) => Verbosity -> ParseFun a -> String -> IO ()
+run v p s =
+  case p ts of
+    Left err -> do
+      putStrV v "\nParse              Failed...\n"
+      putStrV v "Tokens:"
+      mapM_ (putStrV v . showPosToken . mkPosToken) ts
+      putStrLn err
+      exitFailure
+    Right tree  -> do
+      putStrV v "\nParse Successful!"
+      showTree v tree
+      let typecheckResult = runTypechecker tree
+      case typecheckResult of
+        Left err -> do
+          putStrLn "\n## Typechecking Failed...\n"
+          putStrLn err
+          exitFailure
+        Right s -> do
+          putStrV v "\n## Typechecking Successful!"
+        --   putStrV v $ "\n[Final State]\n\n" ++ show s
+          let result = runCompiler tree
+            -- putStrLn (show result)
+          case result of
+            Left err -> do
+              putStrLn "\n## Evaluation Failed...\n"
+              putStrLn err
+              exitFailure
+            Right s -> do
+              putStrV v "\n## Evaluation Successful!"
+              putStrV v $ "\n[Final State]\n\n" ++ show s
+
+              putStrV v "\n[Output]"
+              let lines = unlines $ reverse (compilerOutput s)
+              putStr lines
+              -- exitWith $ exitCode s
+  where
+  ts = myLexer s
+  showPosToken ((l,c),t) = concat [ show l, ":", show c, "\t", show t ]
+
+
+showTree :: (Show a, Print a) => Int -> a -> IO ()
+showTree v tree = do
+  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
+
+usage :: IO ()
+usage = do
+  putStrLn $ unlines
+    [ "usage: Call with one of the following argument combinations:"
+    , "  --help          Display this help message."
+    , "  (no arguments)  Parse stdin verbosely."
+    , "  (files)         Parse content of files verbosely."
+    , "  -s (files)      Silent mode. Parse content of files silently."
+    ]
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["--help"] -> usage
+    []         -> getContents >>= run 0 pProgram
+    "-v":fs    -> mapM_ (runFile 2 pProgram) fs
+    fs         -> mapM_ (runFile 0 pProgram) fs
